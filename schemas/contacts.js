7923d83f9f2755babf36b844c1d0766ab8d00177
angikkate <angikkate@gmail.com>
2023-07-05 10:06:35 +0300

    fix: schemas joi

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,12 @@
 const Joi = require('joi')
 
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const phonePattern = /^[0-9]{10}$/
+
 const addSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required()
+  email: Joi.string().pattern(emailPattern).required(),
+  phone: Joi.string().pattern(phonePattern).required()
 })
 
 module.exports = {
