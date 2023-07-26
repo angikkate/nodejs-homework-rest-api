62ac89f1d702fe45885191774ef606b95500adae
angikkate <angikkate@gmail.com>
2023-07-27 01:28:59 +0300

    node-hw06

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const { handleMongooseError } = require('../helpers')
 
 const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 const subscriptionList = ['starter', 'pro', 'business']
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
 
 const userSchema = new Schema({
   email: {
@@ -30,6 +31,15 @@ const userSchema = new Schema({
   avatarURL: {
     type: String,
     required: true,
+  },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    default: '',
+    required: [true, 'Verify token is required'],
   }
 }, {versionKey: false, timestaps: true})
 
@@ -38,6 +48,10 @@ userSchema.post('save', handleMongooseError)
 const registerSchema = Joi.object({
   email: Joi.string().pattern(emailPattern).required(),
   password: Joi.string().min(6).required(),
+})
+
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
 })
 
 const loginSchema = Joi.object({
@@ -51,6 +65,7 @@ const subscriptionSchema = Joi.object({
 
 const schemas = {
     registerSchema,
+    emailSchema,
     loginSchema,
     subscriptionSchema,
 }
